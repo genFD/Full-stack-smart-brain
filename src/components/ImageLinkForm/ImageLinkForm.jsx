4fe897ad217bf0bad81f86cd09a603d7b32c9870
hermkan <hermann.kanga.dev@gmail.com>
2021-12-03 00:12:10 -0500

    completed logo + navigation components

---
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const ImageLinkForm = () =>{
+ return(
+ <div>
+  <p className='f3'>
+   {"this brain will detect your face"}
+  </p>
+ </div>
+ )
+}
+
+
+export default ImageLinkForm
